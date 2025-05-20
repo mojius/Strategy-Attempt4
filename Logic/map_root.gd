190368774f8fbfd988aef2e2a7f558f9f45d0486
Ben Daly <mvben33@gmail.com>
2025-05-20 13:44:14 -0700

    Broken phase refactors

--- Logic/map_root.gd
+++ Logic/map_root.gd
@@ -8,22 +8,35 @@ func _ready() -> void:
 	add_child(phase_animator)
 	$Cursor.tile_manager = $TileManager
 	while true:
+
+		await player_phase(phase_animator)
+		await enemy_phase(phase_animator)
+		await ally_phase(phase_animator)
+
+
+
+func _process(_delta) -> void:
+	if %Units.find_child("Blue").get_child_count() == 0: get_tree().quit()
+
+
+func player_phase(phase_animator):
 		get_tree().create_timer(0.5)
 		await phase_animator.play(0)
 		get_tree().create_timer(0.5)
 		%Units.refresh_units()
 		$PlayerPhase.run()
 		await $PlayerPhase.turn_ended
+
+func enemy_phase(phase_animator):
 		get_tree().create_timer(0.5)
 		await phase_animator.play(1)
 		get_tree().create_timer(0.5)
 		%Units.refresh_units()
 		await $CPUPhase.run_red()
 		get_tree().create_timer(0.5)
+
+func ally_phase(phase_animator):
 		await phase_animator.play(2)
 		get_tree().create_timer(0.5)
 		%Units.refresh_units()
 		await $CPUPhase.run_green()
-
-func _process(_delta) -> void:
-	if %Units.find_child("Blue").get_child_count() == 0: get_tree().quit()
